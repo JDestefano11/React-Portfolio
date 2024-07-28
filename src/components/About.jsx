da4f972968e555e92137d367ebe1ee5d9dc473a2
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-07-28 13:07:05 -0400

    "Updated About component and styles: removed unused font awesome icons, added new paragraphs and section titles, and modified CSS for skill box and title"

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,7 @@ import {
   FaDatabase,
   FaGitAlt,
   FaAws,
-  FaBrain,
-  FaUserFriends,
-  FaUsers,
-  FaPuzzlePiece,
   FaServer,
-  FaClipboardList,
-  FaHourglassHalf,
   FaDesktop,
   FaCloudDownloadAlt,
   FaGit,
@@ -33,6 +27,28 @@ export const About = () => {
             background in modern technologies like React, Node.js, and MongoDB,
             I strive to deliver high-quality solutions that meet the needs of
             clients and users alike.
+          </p>
+          <p className="description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            euismod, nisl nec tincidunt lacinia, nisl nisl aliquam nisl, eget
+            aliquam nisl nisl eget nisl.
+          </p>
+          <h2 className="section-title">Interests</h2>
+          <p className="description">
+            Exploring new technologies, contributing to open-source projects,
+            and traveling with a focus on nature exploration. I enjoy immersing
+            myself in the latest tech trends, collaborating on community-driven
+            software, discovering new landscapes and wildlife, and experiencing
+            diverse cultures.
+          </p>
+          <h2 className="section-title">Goals</h2>
+          <p className="description">
+            Eager to join a dynamic team to work on innovative projects and grow
+            as a developer. I aim to leverage my skills in technology and
+            passion for open-source contributions to create impactful solutions.
+            Additionally, I strive to continuously learn and adapt to new
+            challenges while fostering a collaborative and inclusive work
+            environment.
           </p>
         </div>
         <h3 className="skills-title">Skills</h3>
